7cba7f79f9452c8b0494a471801788b5cbafb507
Florent Nicolas <flonicolas.pro@outlook.fr>
2019-07-07 00:55:34 +0200

    Admin Dashboard in progress

--- src/Controller/Admin/AdminAdController.php
+++ src/Controller/Admin/AdminAdController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Ad;
+use App\Form\AdType;
 use App\Repository\AdRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -17,5 +21,44 @@ class AdminAdController extends AbstractController
         return $this->render('admin/ad/index.html.twig', [
             'ads' => $ads->findAll()
         ]);
+    }
+
+    /**
+     * @Route("/admin/ads/{id}/edit", name="admin_ads_edit")
+     */
+    public function edit(Ad $ad, Request $request, ObjectManager $manager) {
+        $form = $this->createForm(AdType::class, $ad);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash('success', 'Annonce modifié avec succès');
+        }
+
+        return $this->render('admin/ad/edit.html.twig', [
+            'ad' => $ad,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/ads/{id}/delete", name="admin_ads_delete")
+     */
+    public function delete(Ad $ad, ObjectManager $manager) {
+        $haveActiveBookings = count($ad->getBookings());
+
+        if($haveActiveBookings > 0) {
+            $this->addFlash('warning', 'Impossible de supprimer une annonce avec une ou plusieurs réservation(s) active(s)');
+        }
+        else {
+            $manager->remove($ad);
+            $manager->flush();
+    
+            $this->addFlash('success', 'L\'annonce <em>' . $ad->getTitle() . '</em> a bien été supprimée');
+        }
+
+        return $this->redirectToRoute('admin_ads_index');
     }
 }
---
+++ src/Controller/Admin/AdminCommentController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Comment;
+use App\Form\AdminCommentType;
+use App\Repository\CommentRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdminCommentController extends AbstractController
+{
+    /**
+     * @Route("/admin/comments", name="admin_comments_index")
+     */
+    public function index(CommentRepository $repo)
+    {
+        $comments = $repo->findAll();
+
+        return $this->render('admin/comment/index.html.twig', [
+            'comments' => $comments
+        ]);
+    }
+
+    /**
+     * @Route("/admin/comments/{id}/edit", name="admin_comments_edit")
+     */
+    public function edit(Comment $comment, Request $request, ObjectManager $manager) {
+        $form = $this->createForm(AdminCommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($comment);
+            $manager->flush();
+
+            $this->addFlash('success', 'Commentaire modifié avec succès');
+        }
+
+        return $this->render('admin/comment/edit.html.twig', [
+            'comment' => $comment,
+            'form' => $form->createView()
+        ]);
+    }
+}
